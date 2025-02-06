74bda88d503d752dad833995fb094c55dfa5afc0
mahmoud-mhamed <mahmoud1272022@gmail.com>
2025-02-06 02:22:47 +0200

    feat: Add version endpoint
    Adds a simple `/version` endpoint that returns the
    application's version number (1.0). This allows for easy
    version checking and monitoring.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/version', function () {
+    return 1.0;
+});
 Route::get('/', function () {
     return \Inertia\Inertia::render('Soon');
     return view('welcome');
