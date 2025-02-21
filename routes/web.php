fcdedf0c4108675664e6b496696997861852dbc7
mahmoud-mhamed <mahmoud1272022@gmail.com>
2025-02-21 22:47:40 +0200

    Replace ShowFilterFormResult with FilterForm and improve filtering
    Removed `ShowFilterFormResult` and replaced it with the more flexible `FilterForm` component across the application. Enhanced filtering functionalities by standardizing filter implementations and leveraging builder classes for clean query management. Refactored multiple models and actions to support robust filters and search capabilities.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/version', function () {
-    return 1.3;
+    return 1.4;
 });
 Route::get('/', function () {
     return \Inertia\Inertia::render('Soon');
