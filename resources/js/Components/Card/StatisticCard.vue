a3edf62359ba3e42c8bd735c00ffb5f1223bcae9
mahmoud-mhamed <mahmoud1272022@gmail.com>
2025-04-23 19:09:14 +0200

    Add optional links to statistic cards
    Updated `StatisticCard` to support optional `href` property for linking, defaulting to `null`. Modified dashboard logic to include links in statistic cards and ensured backward compatibility by adjusting related components and helpers.

--- resources/js/Components/Card/StatisticCard.vue
+++ resources/js/Components/Card/StatisticCard.vue
@@ -3,23 +3,28 @@ defineProps({
     title: String,
     value: String,
     icon: String,
+    href: {type: String, default: null},
 })
 </script>
 
 <template>
-    <div class="bg-statistic w-full aspect-[28/7] md:aspect-[18/7] rounded-lg text-primary-300">
-        <div class="flex w-full h-full justify-around items-center">
-            <div class="flex flex-col items-center justify-center text-2xl">
-                <div class="font-bold md:text-6xl" v-text="value" />
-                <div v-text="title" />
-            </div>
-            <div>
-                <div class="border-4 border-primary-300 rounded-full aspect-square md:w-20 p-2 md:p-4 text-center bg-[#F8EAE7]">
-                    <i class="pi md:!text-4xl " :class="icon"></i>
+    <component :is="href?'Link':'div'" :href="href">
+        <div class="bg-statistic w-full aspect-[28/7] md:aspect-[18/7] rounded-lg text-primary-300">
+            <div class="flex w-full h-full justify-around items-center">
+                <div class="flex flex-col items-center justify-center text-2xl">
+                    <div class="font-bold md:text-6xl" v-text="value"/>
+                    <div v-text="title"/>
+                </div>
+                <div>
+                    <div
+                        class="border-4 border-primary-300 rounded-full aspect-square md:w-20 p-2 md:p-4 text-center bg-[#F8EAE7]">
+                        <i class="pi md:!text-4xl " v-if="icon" :class="icon"></i>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </component>
+
 </template>
 
 <style scoped>
